940246449a70c2e4ddc86bc637c281b447ed208f
tkg-blog <128972951+tkg-blog@users.noreply.github.com>
2023-03-27 12:39:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$(".openbtn1").click(function () {
+    $(this).toggleClass('active');
+    $(".blog-nav").toggleClass("active");
+    $("main").toggleClass("hide");
+});
+$("table").attr("cellSpacing","0");
+
+setArticleNav()
+function setArticleNav(){
+    const articleNav=document.getElementById("article-nav");
+    articleNav.innerHTML+="<h2>目次</h2>"
+    articleNav.innerHTML+="<ul>"
+    for(let idx of $("main h1")){
+        if(idx.id!==""){
+            articleNav.innerHTML+=`<li><a href=#${idx.id}>${idx.innerHTML}</a></li>`;
+        }
+    }
+    articleNav.innerHTML+="</ul>";
+}
+for(let dlBtn of document.getElementsByClassName("download")){
+    dlBtn.innerHTML='<img src="./download.svg">ダウンロード';
+}
